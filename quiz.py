313b5bbfd4b2e4f6774f1ebe8a78f0e654a3d796
wicked777 <wicky.akloe@gmail.com>
2019-08-09 12:05:36 +0200

    Created game loop

--- quiz.py
+++ quiz.py
@@ -7,4 +7,18 @@ def show_menu():
     return option
 
 
-print(show_menu())
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("You selected 'Ask questions'")
+        elif option == "2":
+            print("You selected 'Add a question'")
+        elif option == "3":
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+
+game_loop()
