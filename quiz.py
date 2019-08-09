0e7628adc3cb3e2968b3f2a47f9a84879d41b5fa
wicked777 <wicky.akloe@gmail.com>
2019-08-09 12:00:10 +0200

    Created menu selection program

---
+++ quiz.py
@@ -0,0 +1,10 @@
+def show_menu():
+    print("1. Ask questions")
+    print("2. Add a question")
+    print("3. Exit game")
+
+    option = input("Enter option: ")
+    return option
+
+
+print(show_menu())
