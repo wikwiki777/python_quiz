7bc0d518574925de7a56bcdca21929f627356a71
wicked777 <wicky.akloe@gmail.com>
2019-08-09 12:13:48 +0200

    Created option to add question to file

--- quiz.py
+++ quiz.py
@@ -7,13 +7,27 @@ def show_menu():
     return option
 
 
+def add_question():
+    print("")
+    question = input("Enter a questions\n> ")
+
+    print("")
+    print("Ok then, tell me the answer")
+    answer = input("{0}\n> ".format(question))
+
+    file = open("questions.txt", "a")
+    file.write(question + "\n")
+    file.write(answer + "\n")
+    file.close()
+
+
 def game_loop():
     while True:
         option = show_menu()
         if option == "1":
             print("You selected 'Ask questions'")
         elif option == "2":
-            print("You selected 'Add a question'")
+            add_question()
         elif option == "3":
             break
         else:
